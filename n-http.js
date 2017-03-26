2ceeec6af01d12ef40237246a920d3acfac51b76
yanghcc <446573564@qq.com>
2017-03-27 00:16:42 +0800

    http-learn

--- n-http.js
+++ n-http.js
@@ -1,22 +1,45 @@
 /*
 * @Author: hucheng.yang
 * @Date:   2017-03-26 23:05:55
-* @Last Modified by:   hucheng.yang
-* @Last Modified time: 2017-03-26 23:30:27
+* @Last Modified by:   yanghcc
+* @Last Modified time: 2017-03-27 00:15:49
 */
 
 var http = require('http')
-
+var fs = require('fs')
 //创建一个服务器。主要做三件事
 //1.接收请求
 //2.处理请求
 //3.发送响应
 var server = http.createServer();
 server.on('request',function(request,response){
-	console.log(request)
-	console.log('get request')
-	response.write('hello world')
-	response.end()
+	var url = response.url
+	//两个参数：响应码，请求头对象
+
+	if(url = '/css'){
+		response.writeHead(200,{
+			'Content-Type':'text/css'
+		})
+		fs.readFile('./css/hello.css',function(err,data){
+			if (err) {
+				throw err
+			}
+			response.end(data)
+		})
+	}
+	response.writeHead(200,{
+		'Content-Type':'text/html'
+	})
+	fs.readFile('./hello.html',function(err,data){
+		if (err) {
+			throw err
+		}
+		response.end(data)
+	})
+	// console.log(request)
+	// console.log('get request')
+	// response.write('<h2>hello world</h2>')
+	// response.end()
 })
 //绑定一个端口，开启监听
 server.listen(9999,function(){
